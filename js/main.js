4ec05ea3a00bbc8888b84485dc94d5a0640b0132
Mstufing <mstufingms11@gmail.com>
2023-05-14 05:15:22 +0300

    task #1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// ? Task #1
+// ? Alphabet Soup
+
+// function AlphabetSoup(str) {
+//     return str.split("").sort().join("")
+// }
+
+// let myBtn = document.querySelector(".btn");
+
+// myBtn.onclick = () => {
+//     let inp1 = document.querySelector(".inp1");
+//     let myResult = document.querySelector(".result");
+//     myResult.innerHTML = AlphabetSoup(inp1.value);
+// }
+
+// ? Task #2
